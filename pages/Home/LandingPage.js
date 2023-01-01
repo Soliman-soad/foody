b78c2dec7ea988d84dd11242a2d59d6cfaad316f
Soliman-soad <mdsolimansoad@gmail.com>
2023-01-01 11:48:11 +0600

    first commit

---
+++ pages/Home/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from './component/Slider';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Slider/>
+        </div>
+    );
+};
+
+export default LandingPage;
